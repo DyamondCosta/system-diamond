783d06da9738a4a290acef3eb67ccc42ec8b17db
Lucas Alves <lucasalves.acv@gmail.com>
2026-06-03 18:17:40 -0300

    Adiciona editar e excluir usuarios

--- js/usuarios.js
+++ js/usuarios.js
@@ -4,27 +4,63 @@ SUPABASE_URL,
 SUPABASE_ANON_KEY
 );
 
+let usuarioEditando = null;
+
 async function salvarUsuario(){
 
 const nome =
-document.getElementById(
-'nome'
-).value;
+document.getElementById('nome').value;
 
 const email =
-document.getElementById(
-'email'
-).value;
+document.getElementById('email').value;
 
 const senha =
-document.getElementById(
-'senha'
-).value;
+document.getElementById('senha').value;
 
 const perfil =
-document.getElementById(
-'perfil'
-).value;
+document.getElementById('perfil').value;
+
+if(usuarioEditando){
+
+const { error } =
+await clienteSupabase
+.from('usuarios')
+.update({
+nome,
+email,
+senha,
+perfil
+})
+.eq(
+'id',
+usuarioEditando
+);
+
+if(error){
+
+alert(
+'Erro ao atualizar usuário'
+);
+
+console.log(error);
+
+return;
+
+}
+
+alert(
+'Usuário atualizado'
+);
+
+usuarioEditando = null;
+
+limparCampos();
+
+carregarUsuarios();
+
+return;
+
+}
 
 const { error } =
 await clienteSupabase
@@ -55,7 +91,18 @@ alert(
 'Usuário salvo'
 );
 
+limparCampos();
+
 carregarUsuarios();
+
+}
+
+function limparCampos(){
+
+document.getElementById('nome').value='';
+document.getElementById('email').value='';
+document.getElementById('senha').value='';
+document.getElementById('perfil').value='Administrador';
 
 }
 
@@ -97,10 +144,89 @@ ${usuario.email}
 ${usuario.perfil}
 </p>
 
+<button
+onclick="editarUsuario(${usuario.id})">
+✏️ Editar
+</button>
+
+<button
+onclick="excluirUsuario(${usuario.id})">
+🗑️ Excluir
+</button>
+
 </div>
 `;
 
 });
+
+}
+
+async function editarUsuario(id){
+
+const { data } =
+await clienteSupabase
+.from('usuarios')
+.select('*')
+.eq('id', id)
+.single();
+
+if(!data) return;
+
+usuarioEditando = id;
+
+document.getElementById('nome').value =
+data.nome || '';
+
+document.getElementById('email').value =
+data.email || '';
+
+document.getElementById('senha').value =
+data.senha || '';
+
+document.getElementById('perfil').value =
+data.perfil || '';
+
+window.scrollTo({
+top:0,
+behavior:'smooth'
+});
+
+}
+
+async function excluirUsuario(id){
+
+const confirmar =
+confirm(
+'Deseja excluir este usuário?'
+);
+
+if(!confirmar){
+return;
+}
+
+const { error } =
+await clienteSupabase
+.from('usuarios')
+.delete()
+.eq('id', id);
+
+if(error){
+
+alert(
+'Erro ao excluir usuário'
+);
+
+console.log(error);
+
+return;
+
+}
+
+alert(
+'Usuário excluído'
+);
+
+carregarUsuarios();
 
 }
 
